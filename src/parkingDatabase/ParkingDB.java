67bb40651078058db9c712b6a3ebf07f7045870c
Tyler Pitsch <tpitsch@gmail.com>
2018-08-04 19:11:19 -0700

    Update ParkingDB.java

--- src/parkingDatabase/ParkingDB.java
+++ src/parkingDatabase/ParkingDB.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 
 public class ParkingDB {
 	
-	private static String userName = "tpitsch";
-	private static String password = "omRaic";
+	private static String userName = "*******";
+	private static String password = "*******";
 	private static String serverName = "cssgate.insttech.washington.edu";
 	private static Connection sConnection;
 
