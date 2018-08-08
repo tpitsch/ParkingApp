d2be63213906976528c1980c74265a896b99bbf4
Tyler <tpitsch@gmail.com>
2018-08-08 13:50:31 -0700

    doc

--- src/parkingDatabase/ParkingDB.java
+++ src/parkingDatabase/ParkingDB.java
@@ -12,8 +12,8 @@ import modelObjects.StaffSpace;
 public class ParkingDB {
 	
 
-	private static String userName = "*";
-	private static String password = "*";
+	private static String userName = "tpitsch";
+	private static String password = "omRaic";
 	private static String serverName = "cssgate.insttech.washington.edu"; 
 	private static Connection sConnection;
 
